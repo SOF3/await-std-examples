b13df5565a706f3096650ae349eb1ecb2713c7b1
SOFe <sofe2038@gmail.com>
2020-05-10 21:16:08 +0800

    Bugfixes

--- BanHammer/src/SOFe/BanHammer/Main.php
+++ BanHammer/src/SOFe/BanHammer/Main.php
@@ -2,9 +2,11 @@
 
 namespace SOFe\BanHammer;
 
+use Generator;
 use pocketmine\Player;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\plugin\PluginBase;
 use SOFe\AwaitStd\Await;
 use SOFe\AwaitStd\AwaitStd;
@@ -21,16 +23,33 @@ final class Main extends PluginBase {
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
 		\assert($command->getName() === "bh");
 
-		Await::f2c(function() use($sender) : void {
+		Await::f2c(function() use($sender) : Generator {
 			try {
 				if(!($sender instanceof Player)) {
 					$sender->sendMessage("Run this command in-game.");
 					return true;
 				}
 
-				$sender->sendMessage("[BanHammer] Click the player to ban");
+				$sender->sendMessage("[BanHammer] Click the player to ban. Type \"cancel\" in chat to cancel.");
 				while(true) {
-					$event = yield $this->std->nextAttack($sender);
+					// This yield line will suspend until either nextAttack or nextChat happens
+					[$which, $event] = yield Await::race([
+						"attack" => $this->std->nextAttack($sender),
+						"chat" => $this->std->nextChat($sender),
+					]);
+					if($which === "chat") {
+						// nextChat happened first
+						if(strtolower($event->getMessage()) === "cancel") {
+							$event->setCancelled();
+							$sender->sendMessage("BanHammer has been cancelled");
+							return; // we don't need to wait for anything anymore
+						} else {
+							continue; // if player chats, we don't handle it, so we keep on racing attack/chat again
+						}
+					}
+
+					\assert($which === "attack");
+					\assert($event instanceof EntityDamageByEntityEvent);
 					$target = $event->getEntity();
 					if(!($target instanceof Player)) {
 						$sender->sendMessage("You can only ban players. Try clicking agian.");
@@ -41,13 +60,15 @@ final class Main extends PluginBase {
 				}
 				$sender->sendMessage("[BanHammer] Selected {$target->getName()}.");
 				$sender->sendMessage("[BanHammer] Type the ban message.");
-				$message = yield $sender->nextChat();
-				$this->getServer()->getNameBans()->addBan($target->getName(), $message);
+				$message = yield $this->std->consumeNextChat($sender);
+				$this->getServer()->getNameBans()->addBan($target->getName(), $message, null, $sender->getName());
 				$target->kick("Banned: $message");
 				$sender->sendMessage("{$target->getName()} has been banned.");
 			} catch(QuitException $e) {
 				// nothing needs to be done if sender quits
 			}
 		});
+
+		return true; // we don't need that usage message anyway
 	}
 }
